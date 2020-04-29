4e97d14ac106c04e411fa34d7c2e9fcea8705e23
ngh7mr3 <@ngh7mr3 everywhere>
2020-04-30 02:50:18 +0300

    added todo

--- server.py
+++ server.py
@@ -5,12 +5,22 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', type=int, default=8080)
+parser.add_argument('-m', '--mask', type=int, default=24)
+parser.add_argument('-t', '--timeout', type=int, default=120)
 app_settings = parser.parse_args()
 
+# TODO:
+# - add async handler for db
+# - add handler to clear timeout for specifix prefix
+# - add tests (pytest-aiohttp)
+
 async def handler(request):
+	#request_ip = request._message.headers['X-Forwarded-For']
+	#print(request._message.headers)
+	#print(request_ip)
 	return web.Response(text="OK")
 
-async def main(loop):
+async def main():
 	server = web.Server(handler)
 	runner = web.ServerRunner(server)
 	await runner.setup()
@@ -22,7 +32,7 @@ async def main(loop):
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
 	try:
-		loop.run_until_complete(main(loop))
+		loop.run_until_complete(main())
 	except KeyboardInterrupt:
 		pass
 	loop.close()
