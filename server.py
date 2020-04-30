df5959e1f71d17ac26759ef1e181ca661449f77e
ngh7mr3 <@ngh7mr3 everywhere>
2020-04-30 21:52:17 +0300

    created async handler for Redis DB with basic usage

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from aiohttp import web
 import asyncio
 import argparse
+import redis
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', type=int, default=8080)
@@ -13,10 +14,10 @@ tmp_s = bin((2**32-1) & ~(2**(32-service_settings.mask)-1))[2:]
 SERVICE_PORT = service_settings.port
 SERVICE_MASK = [int(i, 2) for i in [tmp_s[8*j:8*(j+1)] for j in range(4)]]
 SERVICE_TIMEOUT = service_settings.timeout
+REDIS = redis.Redis() # using default redis-server at localhost:6379
 
 # TODO:
-# - add async handler for db
-#	Redis seeems to be the best practice, so let's use async python wrapper for it
+# - create logic for rate limiting by IP
 # - add handler to clear timeout for specifix prefix
 # - add tests (pytest-aiohttp)
 # - add docker (?)
@@ -26,12 +27,23 @@ async def bitmask_ip(ip: str) -> str:
 	raw_bytes = [str(i&j) for i,j in zip(ip_bytes, SERVICE_MASK)]
 	return '.'.join(raw_bytes)
 
+async def process_ip(ip: str):
+	masked_ip = await bitmask_ip(ip)
+	if ip in REDIS:
+		print(f"Already known user from {ip}")
+	else:
+		print(f"Unknown user {ip}, registering")
+		REDIS[ip] = 1
+
 async def handler(request):
-	# assume ip provided with every request
-	request_ip = request._message.headers['X-Forwarded-For']
-	masked_ip = await bitmask_ip(request_ip)
+	try:
+		request_ip = request._message.headers['X-Forwarded-For']
+	except KeyError:
+		print("Got header without forwarded ip")
+		request_ip = '0.0.0.0'
+
+	await process_ip(request_ip)
 	print(request._message.headers)
-	print(f"Header IP: {request_ip}, Masked IP: {masked_ip}")
 	return web.Response(text="OK")
 
 async def main():
