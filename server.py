495aec63069de48615dacb9fac6e4bf0afdbc8a6
ngh7mr3 <@ngh7mr3 everywhere>
2020-04-30 17:56:47 +0300

    created global config variables (they could also be stored into web.Application.__dict__, but we use web.Server)

--- server.py
+++ server.py
@@ -7,12 +7,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--port', type=int, default=8080)
 parser.add_argument('-m', '--mask', type=int, default=24)
 parser.add_argument('-t', '--timeout', type=int, default=120)
-app_settings = parser.parse_args()
+service_settings = parser.parse_args()
+
+SERVICE_PORT = service_settings.port
+SERVICE_MASK = [] # list of ints
+SERVICE_TIMEOUT = service_settings.timeout
 
 # TODO:
 # - add async handler for db
+#	Redis seeems to be the best practice, so let's use async python wrapper for it
 # - add handler to clear timeout for specifix prefix
 # - add tests (pytest-aiohttp)
+# - add docker (?)
+
+async def bitmask_ip(ip: str) -> str:
+	pass
 
 async def handler(request):
 	#request_ip = request._message.headers['X-Forwarded-For']
@@ -24,8 +33,10 @@ async def main():
 	server = web.Server(handler)
 	runner = web.ServerRunner(server)
 	await runner.setup()
-	site = web.TCPSite(runner, '0.0.0.0', app_settings.port)
+
+	site = web.TCPSite(runner, '0.0.0.0', SERVICE_PORT)
 	await site.start()
+
 	print(f"===== SERVING on http://0.0.0.0:{app_settings.port}/ =====")
 	await asyncio.sleep(100*3600)
 
