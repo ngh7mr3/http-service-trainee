9caddbf0f6cc30d782919eec901b6bde70231fac
ngh7mr3 <@ngh7mr3 everywhere>
2020-05-02 18:14:40 +0300

    fix layout

--- responses.py
+++ responses.py
@@ -1,11 +1,11 @@
 class SimpleHTMLBody():
 	def __init__(self, title: str, h1: str, p: str):
-		self.title = 	f"<title>{title}</title>"
-		self.h1 = 		f"<h1>{h1}</h1>"
-		self.p =		f"<p>{p}</p>"
-		self.head = 	f"<head>{self.title}</head>"
-		self.body = 	f"<body>{self.h1}\n{self.p}</body>"
-		self.html = 	f"<html>{self.head}\n{self.body}</html>"
+		self.title = f"<title>{title}</title>"
+		self.h1 = f"<h1>{h1}</h1>"
+		self.p = f"<p>{p}</p>"
+		self.head = f"<head>{self.title}</head>"
+		self.body = f"<body>{self.h1}\n{self.p}</body>"
+		self.html = f"<html>{self.head}\n{self.body}</html>"
 
 	def __str__(self):
 		return self.html
