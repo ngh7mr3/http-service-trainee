a692863f55046fb6d63f3b8e5385d5febb547a6c
ngh7mr3 <@ngh7mr3 everywhere>
2020-05-08 16:50:24 +0300

    updated code with pep8 style guides

--- server.py
+++ server.py
@@ -6,140 +6,163 @@ import argparse
 import redis
 import responses
 
+
 async def bitmask_ip(ip_bytes, mask_bytes):
-	raw_bytes = [str(i&j) for i,j in zip(ip_bytes, mask_bytes)]
-	return '.'.join(raw_bytes)
+    raw_bytes = [str(i & j) for i, j in zip(ip_bytes, mask_bytes)]
+    return ".".join(raw_bytes)
+
 
 async def validate_ip(ip: str):
-	try:
-		byte_list = ip.split('.')
-		if len(byte_list) != 4:
-			return False
+    try:
+        byte_list = ip.split(".")
+        if len(byte_list) != 4:
+            return False
 
-		for byte in byte_list:
-			if len(byte)>3 or len(byte)<1 or int(byte)>255 or int(byte)<0:
-				return False
-	except Exception:
-		return False
+        for byte in byte_list:
+            if len(byte) > 3 or len(byte) < 1:
+                return False
+            elif int(byte) > 255 or int(byte) < 0:
+                return False
+    except Exception:
+        return False
 
-	return map(int, byte_list)
+    return map(int, byte_list)
+
 
 async def content_handler(request):
-	_body = request.app['html_body_200']
-	return web.HTTPOk(body = str(_body), content_type = 'text/html')
+    _body = request.app["html_body_200"]
+    return web.HTTPOk(body=str(_body), content_type="text/html")
+
 
 async def reset_timeout_handler(request):
-	try:
-		key = request.query['key']
-		ip  = request.query['ip']
-	except KeyError:
-		raise web.HTTPBadRequest(text = "Provide subnet prefix "
-								"(or ip from specific subnet) "
-								"and secret key to reset timeout. "
-								"Ex.: /reset_timeout?key=foo&ip=1.2.3.4")
-	# validating secret key
-	if key != request.app['secret_key']:
-		raise web.HTTPBadRequest(text = "Unauthorized")
+    try:
+        key = request.query["key"]
+        ip = request.query["ip"]
+    except KeyError:
+        raise web.HTTPBadRequest(
+            text="Provide subnet prefix "
+            "(or ip from specific subnet) "
+            "and secret key to reset timeout. "
+            "Ex.: /reset_timeout?key=foo&ip=1.2.3.4"
+        )
 
-	# validating given prefix/ip
-	prefix = await validate_ip(ip)
-	if prefix == False:
-		raise web.HTTPBadRequest(text = "You should provide valid prefix/ip")
-	
-	# got valid prefix and valid secret key
-	masked_prefix = await bitmask_ip(prefix, request.app['mask'])
-	REDIS_TIMEOUT = request.app['timeout_db']
+    # validating secret key
+    if key != request.app["secret_key"]:
+        raise web.HTTPBadRequest(text="Unauthorized")
 
-	if masked_prefix in REDIS_TIMEOUT:
-		del REDIS_TIMEOUT[masked_prefix]
-		return web.HTTPOk(text = "OK")
-	else:
-		raise web.HTTPBadRequest(text = "Provided prefix isn't timed out")
+    # validating given prefix/ip
+    prefix = await validate_ip(ip)
+    if not prefix:
+        raise web.HTTPBadRequest(text="You should provide valid prefix/ip")
+
+    # got valid prefix and valid secret key
+    masked_prefix = await bitmask_ip(prefix, request.app["mask"])
+    REDIS_TIMEOUT = request.app["timeout_db"]
+
+    if masked_prefix in REDIS_TIMEOUT:
+        del REDIS_TIMEOUT[masked_prefix]
+        return web.HTTPOk(text="OK")
+    else:
+        raise web.HTTPBadRequest(text="Provided prefix isn't timed out")
+
 
 @web.middleware
 async def ip_checkpoint(request, handler):
-	# processing cases with only 1 ip address provided
-	try:
-		_ip = request.headers['X-Forwarded-For']
-		request_ip = await validate_ip(_ip)
-		if request_ip == False:
-			raise web.HTTPBadRequest(text = f"Bad ip provided in header: {_ip}")
-	except KeyError:
-		print("Got header without forwarded ip")
-		ans = "Can't find any IP provided in X-Forwarded-For header"
-		raise web.HTTPBadRequest(text = ans)
-	
-	masked_ip = await bitmask_ip(request_ip, request.app['mask'])
-	print(f"Got request from {masked_ip}")
+    # processing cases with only 1 ip address provided
+    try:
+        _ip = request.headers["X-Forwarded-For"]
+        request_ip = await validate_ip(_ip)
+        if not request_ip:
+            ans = f"Bad ip provided in header: {_ip}"
+            raise web.HTTPBadRequest(text=ans)
+    except KeyError:
+        print("Got header without forwarded ip")
+        ans = "Can't find any IP provided in X-Forwarded-For header"
+        raise web.HTTPBadRequest(text=ans)
 
-	REDIS_SESSION = request.app['session_db']
-	REDIS_TIMEOUT = request.app['timeout_db']
+    masked_ip = await bitmask_ip(request_ip, request.app["mask"])
+    print(f"Got request from {masked_ip}")
 
-	current_timeout = REDIS_TIMEOUT.ttl(masked_ip)
+    REDIS_SESSION = request.app["session_db"]
+    REDIS_TIMEOUT = request.app["timeout_db"]
 
-	_body = request.app['html_body_429']
-	_header = request.app['http_header']
+    current_timeout = REDIS_TIMEOUT.ttl(masked_ip)
 
-	if current_timeout > -1:
-		print(f"Seems like IP {masked_ip} is still timed out for {current_timeout}")
-		_header['Retry-After'] = str(current_timeout)
-		raise web.HTTPTooManyRequests(body = str(_body), headers = _header)
+    _body = request.app["html_body_429"]
+    _header = request.app["http_header"]
 
-	elif masked_ip in REDIS_SESSION:
-		if REDIS_SESSION.decrby(masked_ip, 1) < 0:
-			print(f"{masked_ip} exceeded rate limit and got timeout...")
-			REDIS_TIMEOUT.setex(masked_ip, request.app['timeout'], 1)
+    if current_timeout > -1:
+        print(f"{masked_ip} is still timed out for {current_timeout}")
+        _header["Retry-After"] = str(current_timeout)
+        raise web.HTTPTooManyRequests(body=str(_body), headers=_header)
 
-			# clear user session for case timeout ttl < session ttl
-			del REDIS_SESSION[masked_ip]
+    elif masked_ip in REDIS_SESSION:
+        if REDIS_SESSION.decrby(masked_ip, 1) < 0:
+            print(f"{masked_ip} exceeded rate limit and got timeout...")
+            REDIS_TIMEOUT.setex(masked_ip, request.app["timeout"], 1)
 
-			_header['Retry-After'] = str(request.app['timeout'])
-			raise web.HTTPTooManyRequests(body = str(_body), headers = _header)
-	else:
-		print(f"New IP or last session expired")
-		REDIS_SESSION.setex(masked_ip, 60, request.app['max_requests']-1)
+            # clear user session for case timeout ttl < session ttl
+            del REDIS_SESSION[masked_ip]
 
-	return await handler(request)
+            _header["Retry-After"] = str(request.app["timeout"])
+            raise web.HTTPTooManyRequests(body=str(_body), headers=_header)
+    else:
+        print(f"New IP or last session expired")
+        REDIS_SESSION.setex(masked_ip, 60, request.app["max_requests"] - 1)
 
-def initialize_app(port : int, mask : int, timeout : int, 
-	max_requests : int, redis_session_db : int, redis_timeout_db : int):
-	
-	app = web.Application(middlewares = [ip_checkpoint])
-	app.add_routes([web.route('*', '/foo', content_handler),
-				web.route('*', '/bar', content_handler),
-				web.route('*', '/content', content_handler)])
-	app.add_routes([web.get('/reset_timeout', reset_timeout_handler)])
+    return await handler(request)
 
-	# main settings
-	app['port'] = port
 
-	# store mask as list of integers ex. [255, 255, 255, 0]
-	tmp_s = bin((1<<32) - (1<<32>>mask))[2:]
-	app['mask'] = [int(i, 2) for i in [tmp_s[8*j:8*(j+1)] for j in range(4)]]
+def initialize_app(
+    port: int,
+    mask: int,
+    timeout: int,
+    max_requests: int,
+    redis_session_db: int,
+    redis_timeout_db: int,
+):
 
-	app['timeout'] = timeout
-	app['max_requests'] = max_requests
-	app['session_db'] = redis.Redis(db = redis_session_db)
-	app['timeout_db'] = redis.Redis(db = redis_timeout_db)
+    app = web.Application(middlewares=[ip_checkpoint])
+    app.add_routes(
+        [
+            web.route("*", "/foo", content_handler),
+            web.route("*", "/bar", content_handler),
+            web.route("*", "/content", content_handler),
+        ]
+    )
+    app.add_routes([web.get("/reset_timeout", reset_timeout_handler)])
 
-	# static utils
-	app['html_body_200'] = responses.HTMLResponse200()
-	app['html_body_429'] = responses.HTMLResponse429(max_requests)
-	app['http_header'] = CIMultiDict()
-	app['http_header']['Content-Type'] = 'text/html'
-	app['secret_key'] = 'secret_key'
+    # main settings
+    app["port"] = port
 
-	return app
+    # store mask as list of integers ex. [255, 255, 255, 0]
+    tmp_s = bin((1 << 32) - (1 << 32 >> mask))[2:]
+    app["mask"] = [int(i, 2) for i in [tmp_s[8*j:8*(j+1)] for j in range(4)]]
 
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-p', '--port', type=int, default=8080)
-	parser.add_argument('-m', '--mask', type=int, default=24)
-	parser.add_argument('-t', '--timeout', type=int, default=120)
-	parser.add_argument('-r', '--requests', type=int, default=100)
-	svc_settings = parser.parse_args()
-	
-	# we will use 1st and 2nd redis db for 'production'
-	app = initialize_app(*(vars(svc_settings).values()), 1, 2)
+    app["timeout"] = timeout
+    app["max_requests"] = max_requests
+    app["session_db"] = redis.Redis(db=redis_session_db)
+    app["timeout_db"] = redis.Redis(db=redis_timeout_db)
 
-	web.run_app(app, port = app['port'])
+    # static utils
+    app["html_body_200"] = responses.HTMLResponse200()
+    app["html_body_429"] = responses.HTMLResponse429(max_requests)
+    app["http_header"] = CIMultiDict()
+    app["http_header"]["Content-Type"] = "text/html"
+    app["secret_key"] = "secret_key"
+
+    return app
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("-m", "--mask", type=int, default=24)
+    parser.add_argument("-t", "--timeout", type=int, default=120)
+    parser.add_argument("-r", "--requests", type=int, default=100)
+    svc_settings = parser.parse_args()
+
+    # we will use 1st and 2nd redis db for 'production'
+    app = initialize_app(*(vars(svc_settings).values()), 1, 2)
+
+    web.run_app(app, port=app["port"])
--- test_server.py
+++ test_server.py
@@ -12,195 +12,206 @@ from aiohttp import web
 # We will use another dbs, instead of 'production' db=1 and db=2
 REDIS_SESSION = 0
 REDIS_TIMEOUT = 0
-PATHS = ['/foo', '/bar', '/content']
+PATHS = ["/foo", "/bar", "/content"]
 
 # main service settings: port, mask, timeout, max_requests
 svc_settings = [8080, 24, 120, 100]
 mask = [255, 255, 255, 0]
 
+
 async def flush_db():
-	REDIS_SESSION.flushdb()
-	REDIS_TIMEOUT.flushdb()
+    REDIS_SESSION.flushdb()
+    REDIS_TIMEOUT.flushdb()
+
 
 @pytest.fixture
 async def cli(aiohttp_client):
-	global REDIS_SESSION, REDIS_TIMEOUT
-	app = server.initialize_app(*svc_settings, 3, 4)
-	REDIS_SESSION = app['session_db']
-	REDIS_TIMEOUT = app['timeout_db']
-	await flush_db()
-	return await aiohttp_client(app)
+    global REDIS_SESSION, REDIS_TIMEOUT
+    app = server.initialize_app(*svc_settings, 3, 4)
+    REDIS_SESSION = app["session_db"]
+    REDIS_TIMEOUT = app["timeout_db"]
+    await flush_db()
+    return await aiohttp_client(app)
+
 
 async def test_without_provided_header(cli):
-	resp = await cli.get('/')
-	assert resp.status == 400
-	
-	resp = await cli.get('/foo')
-	assert resp.status == 400
-	
-	resp = await cli.get('/asd')
-	assert resp.status == 400
+    resp = await cli.get("/")
+    assert resp.status == 400
+
+    resp = await cli.get("/foo")
+    assert resp.status == 400
+
+    resp = await cli.get("/asd")
+    assert resp.status == 400
+
 
 async def test_with_invalid_header(cli):
-	header = {'X-Forwarded-For': 'asd'}
+    header = {"X-Forwarded-For": "asd"}
 
-	resp = await cli.get('/', headers = header)
-	assert resp.status == 400
+    resp = await cli.get("/", headers=header)
+    assert resp.status == 400
 
-	resp = await cli.get('/foo', headers = header)
-	assert resp.status == 400
+    resp = await cli.get("/foo", headers=header)
+    assert resp.status == 400
 
-	resp = await cli.get('/asd', headers = header)
-	assert resp.status == 400
+    resp = await cli.get("/asd", headers=header)
+    assert resp.status == 400
+
 
 async def test_with_valid_header(cli):
-	ip = [1,2,3,4]
-	masked_ip = await server.bitmask_ip(ip, mask)
-	header = {'X-Forwarded-For': '1.2.3.4'}
+    ip = [1, 2, 3, 4]
+    masked_ip = await server.bitmask_ip(ip, mask)
+    header = {"X-Forwarded-For": "1.2.3.4"}
 
-	resp = await cli.get('/', headers = header)
-	assert resp.status == 404
-	assert masked_ip in REDIS_SESSION
-	
-	resp = await cli.get('/foo', headers = header)
-	assert resp.status == 200
-	assert masked_ip not in REDIS_TIMEOUT
+    resp = await cli.get("/", headers=header)
+    assert resp.status == 404
+    assert masked_ip in REDIS_SESSION
 
-	resp = await cli.get('/asd', headers = header)
-	assert resp.status == 404
-	assert masked_ip not in REDIS_TIMEOUT
+    resp = await cli.get("/foo", headers=header)
+    assert resp.status == 200
+    assert masked_ip not in REDIS_TIMEOUT
+
+    resp = await cli.get("/asd", headers=header)
+    assert resp.status == 404
+    assert masked_ip not in REDIS_TIMEOUT
+
 
 async def test_ips_from_different_subnets(cli):
-	ips = [[1,1,1,1], [1,1,2,1], [1,2,1,1], [2,1,1,1]]
-	masked_ips = [await server.bitmask_ip(ip, mask) for ip in ips]
-	
-	ok_url_pack = PATHS
-	not_found_url_pack = ['/', '/test', '/admin']
-	packs = [(200, ok_url_pack), (404, not_found_url_pack)]
-	
-	for ip, masked_ip in zip(ips, masked_ips):
-		_ip = '.'.join(map(str, ip))
-		for code, url_pack in packs:
-			for url in url_pack:
-				#ok, this looks terrible, I know
-				resp = await cli.get(url, headers = {'X-Forwarded-For' : _ip})
-				assert resp.status == code
-		# after getting 3 200s and 3 404s
-		# ip from current subnet should be able
-		# to create 100 - 6 = 94 requests
-		assert int(REDIS_SESSION.get(masked_ip)) == 94
+    ips = [[1, 1, 1, 1], [1, 1, 2, 1], [1, 2, 1, 1], [2, 1, 1, 1]]
+    masked_ips = [await server.bitmask_ip(ip, mask) for ip in ips]
+
+    ok_url_pack = PATHS
+    not_found_url_pack = ["/", "/test", "/admin"]
+    packs = [(200, ok_url_pack), (404, not_found_url_pack)]
+
+    for ip, masked_ip in zip(ips, masked_ips):
+        _ip = ".".join(map(str, ip))
+        for code, url_pack in packs:
+            for url in url_pack:
+                # ok, this looks terrible, I know
+                resp = await cli.get(url, headers={"X-Forwarded-For": _ip})
+                assert resp.status == code
+        # after getting 3 200s and 3 404s
+        # ip from current subnet should be able
+        # to create 100 - 6 = 94 requests
+        assert int(REDIS_SESSION.get(masked_ip)) == 94
+
 
 async def test_ips_from_one_subnet(cli):
-	# testing for default subnet mask 255.255.255.0
-	ips = ['1.2.3.100', '1.2.3.255', '1.2.3.0', '1.2.3.4']
-	masked_ip = '1.2.3.0'
-	
-	ok_url_pack = PATHS
-	not_found_pack = ['/', '/set', 'test']
-	packs = [(200, ok_url_pack), (404, not_found_pack)]
+    # testing for default subnet mask 255.255.255.0
+    ips = ["1.2.3.100", "1.2.3.255", "1.2.3.0", "1.2.3.4"]
+    masked_ip = "1.2.3.0"
 
-	for ip in ips:
-		for code, url_pack in packs:
-			for url in url_pack:
-				resp = await cli.get(url, headers = {'X-Forwarded-For': ip})
-				assert resp.status == code
-	
-	# after 24 requests from subnet 1.2.3.0
-	# we should be able to make 100-24=76 requests
-	assert int(REDIS_SESSION.get(masked_ip)) == 76
+    ok_url_pack = PATHS
+    not_found_pack = ["/", "/set", "test"]
+    packs = [(200, ok_url_pack), (404, not_found_pack)]
+
+    for ip in ips:
+        for code, url_pack in packs:
+            for url in url_pack:
+                resp = await cli.get(url, headers={"X-Forwarded-For": ip})
+                assert resp.status == code
+
+    # after 24 requests from subnet 1.2.3.0
+    # we should be able to make 100-24=76 requests
+    assert int(REDIS_SESSION.get(masked_ip)) == 76
+
 
 async def test_timeout_for_one_ip(cli):
-	ip = '1.2.3.4'
-	url = '/foo'
-	
-	# processing 100+ requests under 1 minute
-	# to get a timeout from the server
-	
-	for _ in range(100):
-		resp = await cli.get(url, headers = {'X-Forwarded-For' : ip})
-		assert resp.status == 200
-	
-	# after 101st request we should get timeout for 120 seconds
-	resp = await cli.get(url, headers = {'X-Forwarded-For' : ip})
-	assert resp.status == 429
-	assert resp._headers['Retry-After'] == str(cli.server.app['timeout'])
+    ip = "1.2.3.4"
+    url = "/foo"
+
+    # processing 100+ requests under 1 minute
+    # to get a timeout from the server
+
+    for _ in range(100):
+        resp = await cli.get(url, headers={"X-Forwarded-For": ip})
+        assert resp.status == 200
+
+    # after 101st request we should get timeout for 120 seconds
+    resp = await cli.get(url, headers={"X-Forwarded-For": ip})
+    assert resp.status == 429
+    assert resp._headers["Retry-After"] == str(cli.server.app["timeout"])
+
 
 async def test_timeout_for_one_subnet(cli):
-	ips = ['1.2.3.100', '1.2.3.255', '1.2.3.0', '1.2.3.4']
-	masked_ip = '1.2.3.0'
-	url = '/foo'
+    ips = ["1.2.3.100", "1.2.3.255", "1.2.3.0", "1.2.3.4"]
+    masked_ip = "1.2.3.0"
+    url = "/foo"
 
-	# processing 25 requests for each ip
-	# then test 1 request for each ip (requests: 101, 102, 103 and 104)
-	# they should be timed out with 429 answer
-	for ip in ips:
-		for _ in range(25):
-			resp = await cli.get(url, headers = {'X-Forwarded-For' : ip})
-			assert resp.status == 200
-	
-	# make sure all ips will get timeout after extra requests
-	for ip in ips:
-		resp = await cli.get(url, headers = {'X-Forwarded-For' : ip})
-		assert resp.status == 429
+    # processing 25 requests for each ip
+    # then test 1 request for each ip (requests: 101, 102, 103 and 104)
+    # they should be timed out with 429 answer
+    for ip in ips:
+        for _ in range(25):
+            resp = await cli.get(url, headers={"X-Forwarded-For": ip})
+            assert resp.status == 200
+
+    # make sure all ips will get timeout after extra requests
+    for ip in ips:
+        resp = await cli.get(url, headers={"X-Forwarded-For": ip})
+        assert resp.status == 429
+
 
 async def test_timeout_for_mixed_subnets(cli):
-	subnets = [['1.2.3.1', '1.2.3.2'],['9.8.7.1', '9.8.7.2']]
-	masked_ips = ['1.2.3.0', '9.8.7.0']
-	url = '/foo'
+    subnets = [["1.2.3.1", "1.2.3.2"], ["9.8.7.1", "9.8.7.2"]]
+    masked_ips = ["1.2.3.0", "9.8.7.0"]
+    url = "/foo"
 
-	for _ in range(50):
-		for subnet in subnets:
-			for ip in subnet:
-				resp = await cli.get(url, headers = {'X-Forwarded-For' : ip})
-				assert resp.status == 200
-	
-	# we've made 100 requests from each subnet
-	for ip in masked_ips:
-		assert ip in REDIS_SESSION and ip not in REDIS_TIMEOUT
+    for _ in range(50):
+        for subnet in subnets:
+            for ip in subnet:
+                resp = await cli.get(url, headers={"X-Forwarded-For": ip})
+                assert resp.status == 200
 
-	# let's make extra request from second subnet
-	resp = await cli.get(url, headers = {'X-Forwarded-For' : subnets[1][0]})
-	assert resp.status == 429
-	assert masked_ips[1] in REDIS_TIMEOUT
+    # we've made 100 requests from each subnet
+    for ip in masked_ips:
+        assert ip in REDIS_SESSION and ip not in REDIS_TIMEOUT
+
+    # let's make extra request from second subnet
+    resp = await cli.get(url, headers={"X-Forwarded-For": subnets[1][0]})
+    assert resp.status == 429
+    assert masked_ips[1] in REDIS_TIMEOUT
+
 
 async def test_timeout_reset_query(cli):
-	header = {'X-Forwarded-For' : '1.2.3.4'}
-	url = '/reset_timeout'
-	key = cli.server.app['secret_key']
-	bad_key = 'bad_key'
-	subnet = '1.1.1.1'
-	bad_subnet = '1.a.asd.0001'
+    header = {"X-Forwarded-For": "1.2.3.4"}
+    url = "/reset_timeout"
+    key = cli.server.app["secret_key"]
+    bad_key = "bad_key"
+    subnet = "1.1.1.1"
+    bad_subnet = "1.a.asd.0001"
 
-	# provide only key
-	resp = await cli.get(f"{url}?key={key}", headers = header)
-	assert resp.status == 400
+    # provide only key
+    resp = await cli.get(f"{url}?key={key}", headers=header)
+    assert resp.status == 400
 
-	# provide only subnet
-	resp = await cli.get(f"{url}?ip={subnet}", headers = header)
-	assert resp.status == 400
+    # provide only subnet
+    resp = await cli.get(f"{url}?ip={subnet}", headers=header)
+    assert resp.status == 400
 
-	# provide bad key
-	resp = await cli.get(f"{url}?key={bad_key}&ip={subnet}", headers = header)
-	assert resp.status == 400
+    # provide bad key
+    resp = await cli.get(f"{url}?key={bad_key}&ip={subnet}", headers=header)
+    assert resp.status == 400
 
-	# provide bad subnet
-	resp = await cli.get(f"{url}?key={key}&ip={bad_subnet}", headers = header)
-	assert resp.status == 400
+    # provide bad subnet
+    resp = await cli.get(f"{url}?key={key}&ip={bad_subnet}", headers=header)
+    assert resp.status == 400
 
-	# provide valid key and subnet
-	# but provided prefix isn't timed out
-	resp = await cli.get(f"{url}?key={key}&ip={subnet}", headers = header)
-	assert resp.status == 400
+    # provide valid key and subnet
+    # but provided prefix isn't timed out
+    resp = await cli.get(f"{url}?key={key}&ip={subnet}", headers=header)
+    assert resp.status == 400
 
-	# timeout given subnet
-	REDIS_TIMEOUT.setex('1.1.1.0', cli.server.app['timeout'], 1)
-	# trying to request something
-	resp = await cli.get('/',headers = {'X-Forwarded-For' : subnet})
-	assert resp.status == 429
+    # timeout given subnet
+    REDIS_TIMEOUT.setex("1.1.1.0", cli.server.app["timeout"], 1)
+    # trying to request something
+    resp = await cli.get("/", headers={"X-Forwarded-For": subnet})
+    assert resp.status == 429
 
-	resp = await cli.get(f"{url}?key={key}&ip={subnet}", headers = header)
-	assert resp.status == 200
+    resp = await cli.get(f"{url}?key={key}&ip={subnet}", headers=header)
+    assert resp.status == 200
 
-	# trying to request after resetting timeout
-	resp = await cli.get('/foo', headers = {'X-Forwarded-For' : subnet})
-	assert resp.status == 200
+    # trying to request after resetting timeout
+    resp = await cli.get("/foo", headers={"X-Forwarded-For": subnet})
+    assert resp.status == 200
